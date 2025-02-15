7c6b073149f93e0969b80ef3daec925e4c86dfcb
Marcus Quitiquit <quitiquit04@gmail.com>
2025-02-15 18:45:00 -0500

    Initial Commit

---
+++ Group46_ShoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Group46_ShoppingList
+//
+//  Created by Marcus Quitiquit on 2025-02-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
