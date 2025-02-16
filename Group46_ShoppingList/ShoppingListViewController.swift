05f76dce1ea2fe544a5c4b0bac008e7f5d4409cd
Marcus Quitiquit <quitiquit04@gmail.com>
2025-02-15 21:08:38 -0500

    added shoppinglistviewcontroller

---
+++ Group46_ShoppingList/ShoppingListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ShoppingListViewController.swift
+//  Group46_ShoppingList
+//
+//  Created by Marcus Quitiquit on 2025-02-15.
+//
+
+import UIKit
+
+class ShoppingListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBOutlet weak var navbar: UINavigationBar!
+    
+    
+    @IBOutlet weak var addbutton: UINavigationBar!
+    
+    @IBOutlet weak var totalprice: UILabel!
+    
+    
+    
+    @IBOutlet var table: UITableView!
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
