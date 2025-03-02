6de18dfcc2926bde96dd41804f5395fa610ca5b4
KadirCinar9 <lycan.dodurga1986@gmail.com>
2025-03-01 19:16:17 -0500

    Buttons added

--- Group46_ShoppingList/AddItemViewController.swift
+++ Group46_ShoppingList/AddItemViewController.swift
@@ -8,28 +8,53 @@
 import UIKit
 
 class AddItemViewController: UIViewController {
-
+    
+    // Outlets for Text Fields
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var typeTextField: UITextField!
+    @IBOutlet weak var taxTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Add button to the navigation bar
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(goToAddCategory))
     }
-    
     
     @objc func goToAddCategory(_ sender: Any) {
         performSegue(withIdentifier: "goToAddCategory", sender: self)
     }
     
-    
+    // "Add" Button Action
     @IBAction func add_Item(_ sender: Any) {
+        guard let name = nameTextField.text, !name.isEmpty,
+              let priceString = priceTextField.text, let price = Double(priceString),
+              let type = typeTextField.text, !type.isEmpty,
+              let taxString = taxTextField.text, let tax = Double(taxString) else {
+            showAlert(message: "Please fill in all fields correctly.")
+            return
+        }
         
+        let fullPrice = price + (price * tax / 100) // Calculating final price
+        
+        print("Added Item:")
+        print("Name: \(name), Price: \(price), Type: \(type), Tax: \(tax), Full Price: \(fullPrice)")
+        
+        // If you want to pass data back to the previous screen, use delegation or NotificationCenter
+        dismiss(animated: true, completion: nil)
     }
     
-    
+    // "Go Back" Button Action
     @IBAction func go_Back(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
-    
+    // Helper function to show alerts
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
 }
+
