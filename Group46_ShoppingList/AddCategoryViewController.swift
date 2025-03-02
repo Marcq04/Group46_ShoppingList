2e584c5c074923900ee5e6fae237e2bf7563ede5
Marcus Quitiquit <quitiquit04@gmail.com>
2025-03-02 17:31:22 -0500

    added category button

--- Group46_ShoppingList/AddCategoryViewController.swift
+++ Group46_ShoppingList/AddCategoryViewController.swift
@@ -1,34 +1,55 @@
-//
-//  AddCategoryViewController.swift
-//  Group46_ShoppingList
-//
-//  Created by Marcus Quitiquit on 2025-02-16.
-//
-
 import UIKit
 
+protocol AddCategoryDelegate: AnyObject {
+    func didAddCategory(_ category: String)
+}
+
 class AddCategoryViewController: UIViewController {
+    
+    @IBOutlet weak var categoryInput: UITextField!
+    @IBOutlet weak var categoryStackView: UIStackView! // Reference to StackView inside UIScrollView
+    
+    weak var delegate: AddCategoryDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-    
+
     @IBAction func go_Back(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
 
+    @IBAction func add_Category(_ sender: Any) {
+        guard let categoryName = categoryInput.text, !categoryName.isEmpty else {
+            showAlert(message: "Please enter a category name.")
+            return
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        print("Added category: \(categoryName)")
+        
+        // Add new category to the ScrollView's StackView
+        addCategoryToScrollView(categoryName)
+        
+        delegate?.didAddCategory(categoryName)
     }
-    */
 
+    private func addCategoryToScrollView(_ categoryName: String) {
+        let categoryLabel = UILabel()
+        categoryLabel.text = categoryName
+        categoryLabel.textAlignment = .center
+        categoryLabel.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        categoryLabel.backgroundColor = UIColor.lightGray.withAlphaComponent(0.3)
+        categoryLabel.layer.cornerRadius = 5
+        categoryLabel.clipsToBounds = true
+        categoryLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        categoryStackView.addArrangedSubview(categoryLabel) // Add label to StackView
+    }
+
+    private func showAlert(message: String) {
+        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true)
+    }
 }
